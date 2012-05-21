79123a29c8ecdf0d574c37642cecf9fbc297d118
Trey Bean <trey@12spokes.com>
2012-05-21 17:07:48 -0600

    Remove author_resources
    It didn't seem to be used and caused an error when deleting a podcast.

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -9,8 +9,6 @@ class Podcast < ActiveRecord::Base
   acts_as_taggable_on :genres, :content_types, :topics
 
   belongs_to :photo, :class_name => 'Image'
-  #not sure what these are
-  has_many :author_resources, :as => :favoriteable, :dependent => :destroy
   default_scope :order => "published DESC"
 
   FEED_FIELDS = %w(podcast_title podcast_author podcast_explicit_flag podcast_description podcast_owner_name podcast_owner_email podcast_category podcast_subcategory)
